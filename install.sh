140bd6a9c6ddfd02dc14345eee87c2072abec304
EGAWA Hiroki <niconico_door@gmail.com>
2017-10-24 21:57:20 +0900

    add .colorrc

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@ mkdir -p ~/.vim/backupdir
 mkdir -p ~/.vim/directory
 mkdir -p ~/.vim/undodir
 
+touch ~/.bashrc_local
+
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/dein.toml ~/.vim/dein/rc/dein.toml
 ln -sf ~/.dotfiles/dein_lazy.toml ~/.vim/dein/rc/dein_lazy.toml
+ln -sf ~/.dotfiles/.colorrc ~/.colorrc

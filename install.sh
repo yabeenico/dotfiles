42686e9deff0a40122941988cef1a7058a6675cd
EGAWA Hiroki <niconico_door@gmail.com>
2018-12-09 07:22:09 +0900

    install.sh: .screenrc_local

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ln -sf ~/.dotfiles/.bashrc          ~/
-ln -sf ~/.dotfiles/.colorrc         ~/
-ln -sf ~/.dotfiles/.gitconfig       ~/
-ln -sf ~/.dotfiles/.inputrc         ~/
-ln -sf ~/.dotfiles/.screenrc        ~/
-ln -sf ~/.dotfiles/.screenrc.multi  ~/
-ln -sf ~/.dotfiles/.vimrc           ~/
-ln -sf ~/.dotfiles/.vim             ~/
-
+ln -sf ~/.dotfiles/.bashrc         ~/
+ln -sf ~/.dotfiles/.colorrc        ~/
+ln -sf ~/.dotfiles/.gitconfig      ~/
+ln -sf ~/.dotfiles/.inputrc        ~/
+ln -sf ~/.dotfiles/.screenrc       ~/
+ln -sf ~/.dotfiles/.vimrc          ~/
+ln -sf ~/.dotfiles/.vim            ~/
+touch ~/.dotfiles/.bashrclocal
+touch ~/.dotfiles/.screenrclocal

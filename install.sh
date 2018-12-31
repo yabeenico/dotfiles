d8b1b5532a7636908fb31fd50d9913ec595a81d1
EGAWA Hiroki <niconico_door@gmail.com>
2018-12-31 18:09:34 +0900

    .tmux.conf: add

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ ln -sf ~/.dotfiles/.colorrc        ~/
 ln -sf ~/.dotfiles/.gitconfig      ~/
 ln -sf ~/.dotfiles/.inputrc        ~/
 ln -sf ~/.dotfiles/.screenrc       ~/
-ln -sf ~/.dotfiles/.vimrc          ~/
+ln -sf ~/.dotfiles/.tmux.conf      ~/
 ln -sf ~/.dotfiles/.vim            ~/
-touch ~/.dotfiles/.bashrclocal
-touch ~/.dotfiles/.screenrclocal
+ln -sf ~/.dotfiles/.vimrc          ~/
